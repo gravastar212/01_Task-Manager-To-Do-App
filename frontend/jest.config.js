ad9a5c62af87703e5e5d48f3a068f3f887291a94
gravastar212 <gravastar1992212@gmail.com>
2025-09-24 20:08:30 +0200

    bug fixed

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,8 +1,9 @@
 export default {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-  moduleNameMapping: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'jest-transform-stub'
   },
   transform: {
     '^.+\\.(js|jsx)$': 'babel-jest'
--- frontend/src/__tests__/basic.test.js
+++
@@ -1,6 +0,0 @@
-// Simple test to verify Jest setup
-describe('Basic Test', () => {
-  it('should pass', () => {
-    expect(1 + 1).toBe(2);
-  });
-});
